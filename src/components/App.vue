ff8b37d4837385f217834a3ec5d4905ab0deafc6
Joel James <me@joelsays.com>
2020-05-17 12:47:42 +0530

    👌 Improve code

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container">
       <form>
-        <h1>Contactless WhatsApp</h1>
+        <h1>{{ title }}</h1>
         <div :class="formGroupClass">
           <input type="text" required="required" v-model="number" />
           <label for="input" class="control-label">Phone Number</label>
@@ -25,28 +25,44 @@
 export default {
   name: 'App',
 
-  props: {
-    msg: String
-  },
-
   data() {
     return {
       number: '',
-      error: false
+      error: false,
+      title: 'Contactless WhatsApp'
     };
   },
 
   created() {
-    document.title = 'Contactless WhatsApp';
+    // Set the page title.
+    document.title = this.title;
   },
 
   watch: {
+    /**
+     * When the phone number is changed.
+     *
+     * Remove the error flag.
+     *
+     * @since 1.0.0
+     */
     number() {
       this.error = false;
     }
   },
 
   computed: {
+    /**
+     * Get the class for the form group div.
+     *
+     * Based on the conditions:
+     * - Validation error - `has-error`
+     * - Default - `form-group`
+     *
+     * @since 1.0.0
+     *
+     * @returns {object}
+     */
     formGroupClass() {
       return {
         'form-group': true,
@@ -56,20 +72,41 @@ export default {
   },
 
   methods: {
+    /**
+     * Process the form submit.
+     *
+     * If the number is valid, open it in WhatsApp.
+     *
+     * @since 1.0.0
+     */
     openWhatsApp() {
+      // If valid.
       if (this.validate()) {
         this.error = false;
-        window.location.href =
-          'https://api.whatsapp.com/send?phone=' + this.number;
+        // Redirect to whatsapp.
+        window.location.href = 'https://wa.me/' + this.number;
       } else {
+        // Or set error.
         this.error = true;
       }
     },
 
+    /**
+     * Reset the error flag.
+     *
+     * @since 1.0.0
+     */
     resetError() {
       this.error = false;
     },
 
+    /**
+     * Validate the current form state.
+     *
+     * @since 1.0.0
+     *
+     * @returns {boolean}
+     */
     validate() {
       return this.number.match(/^\d{12}$/);
     }
