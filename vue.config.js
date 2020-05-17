ded3e6654cdd4866334ac1d3e427babe39ba5540
Joel James <me@joelsays.com>
2020-05-17 22:10:57 +0530

    👌 Add config for PWA

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     productionSourceMap: false,
     outputDir: 'docs',
+    pwa: {
+        name: 'Contactless WhatsApp',
+        themeColor: '#4ac959',
+        appleMobileWebAppCapable: 'yes',
+    }
 }
