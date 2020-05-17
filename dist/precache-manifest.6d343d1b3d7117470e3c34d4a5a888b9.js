8557dec783c4b9973fe31919019ac7b5188d7007
Joel James <me@joelsays.com>
2020-05-17 12:24:29 +0530

    📦  Add dist folder

---
+++ dist/precache-manifest.6d343d1b3d7117470e3c34d4a5a888b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d713134277172a4d01",
+    "url": "/css/app.74f4bb3b.css"
+  },
+  {
+    "revision": "751db000695aaafe103b516b50d0b3f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5d713134277172a4d01",
+    "url": "/js/app.5a81d656.js"
+  },
+  {
+    "revision": "8373ad388054c6104742",
+    "url": "/js/chunk-vendors.53062de1.js"
+  },
+  {
+    "revision": "2070c3ba61f9a0b5f06f76724de7ac99",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
